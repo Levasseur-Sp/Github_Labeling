ab8ce8c7b7cffaf5a3e1571581426fe53a8b16de
Levasseur-Sp <levasseur-sp@pm.me>
2019-05-29 12:02:07 -0400

    Modify script
    DRY it by assigning headers url and the label data

--- create_github_labels.sh
+++ create_github_labels.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-# This is a modification from https://gist.github.com/omegahm/28d87a4e1411c030aa89
-# Colours and convention picked from https://robinpowered.com/blog/best-practice-system-for-organizing-and-tagging-github-issues/
 
+# This is a clone of 
+#   https://gist.github.com/Chompas/fb158eb01204d03f783d
+# Colours and convention picked from
+#   https://robinpowered.com/blog/best-practice-system-for-organizing-and-tagging-github-issues/
 
 ## Instructions
 # Bash 4+ is needed because of associative arrays
 # jq (https://stedolan.github.io/jq/)
-# Create access Token from Github (https://help.github.com/articles/creating-an-access-token-for-command-line-use/) and save it under ".token" file
-# Comment / Uncomment / Add, all the labels you want
+# Create access Token from Github https://help.github.com/articles/creating-an-access-token-for-command-line-use/
+#   and save it under ".token" file
+
 # To run the script from command line:
 # $ chmod +x create_labels.sh
 # $ ./create_labels.sh
@@ -17,10 +20,6 @@
 ###
 declare -A LABELS
 
-# Platform
-# LABELS["ruby"]="BFD4F2"
-# LABELS["rails"]="BFD4F2"
-
 # Problems
 LABELS["bug"]="EE3F46"
 LABELS["security"]="EE3F46"
@@ -28,68 +27,60 @@ LABELS["production"]="F45D43"
 
 # Mindless
 LABELS["chore"]="FEF2C0"
-# LABELS["legal"]="FFF2C1"
-
-# Experience
-LABELS["copy"]="FFC274"
-LABELS["design"]="FFC274"
-LABELS["ux"]="FFC274"
-
-# Environment
-# LABELS["staging"]="FAD8C7"
-# LABELS["test"]="FAD8C7"
 
 # Feedback
 LABELS["discussion"]="CC317C"
-# LABELS["rfc"]="CC317C"
-# LABELS["question"]="CC317C"
+LABELS["rfc"]="CC317C"
+LABELS["question"]="CC317C"
 
 # Improvements
 LABELS["enhancement"]="5EBEFF"
-#LABELS["optimizaiton"]="5EBEFF"
+LABELS["optimizaiton"]="5EBEFF"
 
 # Additions
 LABELS["feature"]="91CA55"
 
 # Pending
-LABELS["in progress"]="FBCA04"
-# LABELS["watchlist"]="FBCA04"
+LABELS["in_progress"]="FBCA04"
 
 # Inactive
 LABELS["invalid"]="D2DAE1"
 LABELS["wontfix"]="D2DAE1"
 LABELS["duplicate"]="D2DAE1"
-#LABELS["on hold"]="D2DAE1"
+LABELS["on_hold"]="D2DAE1"
 
 ###
 # Get a token from Github
 ###
 
-TOKEN=$(cat .token)
-
+token=$(cat .token)
 read -p "Who owns the repo you want labels on?: " owner
 read -p "What repo do you want labels on?: " repo
 
-for K in "${!LABELS[@]}"; do
-  CURL_OUTPUT=$(curl -s -H "Authorization: token $TOKEN" -X POST "https://api.github.com/repos/$owner/$repo/labels" -d "{\"name\":\"$K\", \"color\":\"${LABELS[$K]}\"}")
-  HAS_ERROR=$(echo "$CURL_OUTPUT" | jq -r '.errors')
+url="https://api.github.com/repos/${owner}/${repo}/labels"
+header="Authorization: token ${token}"
 
-  if [ ! -z "$HAS_ERROR" ] && [ "$HAS_ERROR" != null ]; then
-    ERROR=$(echo "$CURL_OUTPUT" | jq -r '.errors[0].code')
+for name in ${!LABELS[@]}; do
+    data="{\"name\":\"${name}\", \"color\":\"${LABELS[$name]}\"}"
 
-    if [ "$ERROR" == "already_exists" ]; then
-      # We update
-      echo "'$K' already exists. Updating..."
-      CURL_OUTPUT=$(curl -s -H "Authorization: token $TOKEN" -X PATCH "https://api.github.com/repos/$owner/$repo/labels/${K/ /%20}" -d "{\"name\":\"$K\", \"color\":\"${LABELS[$K]}\"}")
+    curl_output=$(curl -s -H "${header}" -X POST "$url" -d "${data}")
+    curl_has_error=$(echo "${curl_output}" | jq -r '.errors')
+
+    if [ ! -z "${curl_has_error}" ] && [ "${curl_has_error}" != null ]; then
+        error=$(echo "${curl_output}" | jq -r '.errors[0].code')
+
+        if [ "${error}" == "already_exists" ]; then
+            echo "'${name}' already exists. Updating..."
+            curl_output=$(curl -s -H "${header}" -X PATCH "${url}/${name/ /%20}" -d "${data}")
+            echo "Updated '${name}'."
+        else
+            echo "Unknown error: ${error}"
+            echo -e "Output from curl: \n${curl_output}"
+            echo "Exiting..."
+            exit 1
+        fi
     else
-      echo "Unknown error: $ERROR"
-      echo "Output from curl: "
-      echo "$CURL_OUTPUT"
-      echo "Exiting..."
-      exit 1;
+        echo "Created '${name}'."
     fi
-  else
-    echo "Created '$K'."
-  fi
 done
 exit 0
