c79ae9631995d42821b282f5f46303d239948374
Levasseur-Sp <levasseur-sp@pm.me>
2019-05-29 13:07:07 -0400

    Add label descriptions
    use arrays instead of one associative array that can only have one value
    per key unlike the goal of having 3 values

--- create_github_labels.sh
+++ create_github_labels.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This is a clone of 
+# This is a clone of
 #   https://gist.github.com/Chompas/fb158eb01204d03f783d
 # Colours and convention picked from
 #   https://robinpowered.com/blog/best-practice-system-for-organizing-and-tagging-github-issues/
@@ -18,36 +18,21 @@
 ###
 # Label definitions
 ###
-declare -A LABELS
+declare -a names descriptions colors
 
-# Problems
-LABELS["bug"]="EE3F46"
-LABELS["security"]="EE3F46"
-LABELS["production"]="F45D43"
-
-# Mindless
-LABELS["chore"]="FEF2C0"
-
-# Feedback
-LABELS["discussion"]="CC317C"
-LABELS["rfc"]="CC317C"
-LABELS["question"]="CC317C"
-
-# Improvements
-LABELS["enhancement"]="5EBEFF"
-LABELS["optimizaiton"]="5EBEFF"
-
-# Additions
-LABELS["feature"]="91CA55"
-
-# Pending
-LABELS["in_progress"]="FBCA04"
-
-# Inactive
-LABELS["invalid"]="D2DAE1"
-LABELS["wontfix"]="D2DAE1"
-LABELS["duplicate"]="D2DAE1"
-LABELS["on_hold"]="D2DAE1"
+names+=("bug"); descriptions+=("Something isn't right"); colors+=("EE3F46")
+names+=("security"); descriptions+=("Potential Breach"); colors+=("EE3F46")
+names+=("production"); descriptions+=("In production"); colors+=("F45D43")
+names+=("chore"); descriptions+=("Annoying but useful"); colors+=("FEF2C0")
+names+=("discussion"); descriptions+=("Let's talk about it"); colors+=("CC317C")
+names+=("question"); descriptions+=(""); colors+=("CC317C")
+names+=("enhancement"); descriptions+=("Add functionality to an already existing feature"); colors+=("5EBEFF")
+names+=("optimizaiton"); descriptions+=("Optimize code"); colors+=("5EBEFF")
+names+=("feature"); descriptions+=("New feature"); colors+=("91CA55")
+names+=("in progress"); descriptions+=("Currently working on it"); colors+=("FBCA04")
+names+=("wontfix"); descriptions+=("Not important"); colors+=("D2DAE1")
+names+=("duplicate"); descriptions+=("Issue already exists"); colors+=("D2DAE1")
+names+=("on hold"); descriptions+=("Not working on it at the moment"); colors+=("D2DAE1")
 
 ###
 # Get a token from Github
@@ -60,8 +45,8 @@ read -p "What repo do you want labels on?: " repo
 url="https://api.github.com/repos/${owner}/${repo}/labels"
 header="Authorization: token ${token}"
 
-for name in ${!LABELS[@]}; do
-    data="{\"name\":\"${name}\", \"color\":\"${LABELS[$name]}\"}"
+for i in ${!names[@]}; do
+    data="{\"name\":\"${names[$i]}\", \"color\":\"${colors[$i]}\", \"description\":\"${descriptions[$i]}\"}"
 
     curl_output=$(curl -s -H "${header}" -X POST "$url" -d "${data}")
     curl_has_error=$(echo "${curl_output}" | jq -r '.errors')
@@ -70,9 +55,9 @@ for name in ${!LABELS[@]}; do
         error=$(echo "${curl_output}" | jq -r '.errors[0].code')
 
         if [ "${error}" == "already_exists" ]; then
-            echo "'${name}' already exists. Updating..."
-            curl_output=$(curl -s -H "${header}" -X PATCH "${url}/${name/ /%20}" -d "${data}")
-            echo "Updated '${name}'."
+            echo "'${names[$i]}' already exists. Updating..."
+            curl_output=$(curl -s -H "${header}" -X PATCH "${url}/${names[$i]/ /%20}" -d "${data}")
+            echo "Updated '${names[$i]}'."
         else
             echo "Unknown error: ${error}"
             echo -e "Output from curl: \n${curl_output}"
@@ -80,7 +65,7 @@ for name in ${!LABELS[@]}; do
             exit 1
         fi
     else
-        echo "Created '${name}'."
+        echo "Created '${names[$i]}'."
     fi
 done
 exit 0
