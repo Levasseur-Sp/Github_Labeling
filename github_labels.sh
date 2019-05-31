acdf251168517bc8434cc909c43c07d42c6b848c
Levasseur-Sp <levasseur-sp@pm.me>
2019-05-31 15:21:41 -0400

    Make a create_or_update function
    Fix the syntax of the questions.

--- github_labels.sh
+++ github_labels.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 ###
-# Setup of necessary information
+# Setup
 ###
-
 token=$(cat .github_api_token)
-read -p "Who owns the repo you want labels on?: " owner
-read -p "What repo do you want labels on?: " repo
+read -p "What is the name of the repository?: " repo
+read -p "Who is the owner of the repository?: " owner
 
-# url="https://api.github.com/repos/${owner}/${repo}/labels"
-url="https://api.github.com/repos/Levasseur-Sp/Chatting_App/labels"
+url="https://api.github.com/repos/${owner}/${repo}/labels"
 token_header="Authorization: token ${token}"
 accept_header="Accept: application/vnd.github.symmetra-preview+json"
 
@@ -18,8 +16,8 @@ accept_header="Accept: application/vnd.github.symmetra-preview+json"
 ###
 usage() {
     echo "Usage:"
-    echo -e " $0 [ -d | --delete-original ] [ -D | --delete-all ]\n $0 [ --help | -h ]\n"
-    echo -e "\tDefault only creates/updates labels"
+    echo -e " $0 [ -d | --delete-original ] [ --help | -h ]\n"
+    echo -e "\tDefault only creates or updates labels"
 }
 
 ###
@@ -43,11 +41,55 @@ delete_original() {
         sleep 1
     done
     echo "Deleted originals."
-    exit 0
 }
 
 ###
-# Script flags consideration
+# Creation/Updata function
+###
+create_or_update() {
+    declare -a names descriptions colors
+
+    names+=("bug"); descriptions+=("Something isn't working"); colors+=("EE3F46")
+    names+=("security"); descriptions+=("Potential breach"); colors+=("EE3F46")
+    names+=("production"); descriptions+=("In production"); colors+=("F45D43")
+    names+=("chore"); descriptions+=("Annoying to do but needed"); colors+=("FEF2C0")
+    names+=("discussion"); descriptions+=("Communication is key"); colors+=("CC317C")
+    names+=("question"); descriptions+=("Further information is requested"); colors+=("CC317C")
+    names+=("enhancement"); descriptions+=("Add functionality to an already existing feature"); colors+=("5EBEFF")
+    names+=("optimizaiton"); descriptions+=("Code optimization or refactoring"); colors+=("5EBEFF")
+    names+=("feature"); descriptions+=("New feature or request"); colors+=("91CA55")
+    names+=("in progress"); descriptions+=("Currently working on it"); colors+=("FBCA04")
+    names+=("wontfix"); descriptions+=("This will not be worked on"); colors+=("D2DAE1")
+    names+=("duplicate"); descriptions+=("This issue or pull request already exists"); colors+=("D2DAE1")
+    names+=("on hold"); descriptions+=("Not working on it at the moment"); colors+=("D2DAE1")
+
+    for i in ${!names[@]}; do
+        data="{\"name\":\"${names[$i]}\", \"color\":\"${colors[$i]}\", \"description\":\"${descriptions[$i]}\"}"
+
+        curl_output=$(curl -s -H "${token_header}" -H "${accept_header}" -X POST "${url}" -d "${data}")
+        curl_has_error=$(echo "${curl_output}" | jq -r '.errors')
+
+        if [ ! -z "${curl_has_error}" ] && [ "${curl_has_error}" != null ]; then
+            error=$(echo "${curl_output}" | jq -r '.errors[0].code')
+
+            if [ "${error}" == "already_exists" ]; then
+                echo "'${names[$i]}' already exists. Updating..."
+                curl_output=$(curl -s -H "${token_header}" -H "${accept_header}" -X PATCH "${url}/${names[$i]/ /%20}" -d "${data}")
+                echo "Updated '${names[$i]}'."
+            else
+                echo "Unknown error: ${error}"
+                echo -e "Output from curl: \n${curl_output}"
+                echo "Exiting..."
+                exit 1
+            fi
+        else
+            echo "Created '${names[$i]}'."
+        fi
+    done
+}
+
+###
+# Main
 ###
 optspec="dh"
 while getopts "$optspec" opt; do
@@ -64,62 +106,5 @@ while getopts "$optspec" opt; do
     esac
 done
 
-###
-# Label definitions
-###
-declare -a names descriptions colors
-
-names+=("bug"); descriptions+=("Something isn't working"); colors+=("EE3F46")
-names+=("security"); descriptions+=("Potential breach"); colors+=("EE3F46")
-names+=("production"); descriptions+=("In production"); colors+=("F45D43")
-names+=("chore"); descriptions+=("Annoying to do but needed"); colors+=("FEF2C0")
-names+=("discussion"); descriptions+=("Communication is key"); colors+=("CC317C")
-names+=("question"); descriptions+=("Further information is requested"); colors+=("CC317C")
-names+=("enhancement"); descriptions+=("Add functionality to an already existing feature"); colors+=("5EBEFF")
-names+=("optimizaiton"); descriptions+=("Code optimization or refactoring"); colors+=("5EBEFF")
-names+=("feature"); descriptions+=("New feature or request"); colors+=("91CA55")
-names+=("in progress"); descriptions+=("Currently working on it"); colors+=("FBCA04")
-names+=("wontfix"); descriptions+=("This will not be worked on"); colors+=("D2DAE1")
-names+=("duplicate"); descriptions+=("This issue or pull request already exists"); colors+=("D2DAE1")
-names+=("on hold"); descriptions+=("Not working on it at the moment"); colors+=("D2DAE1")
-
-###
-# Get and set necessary information
-###
-
-token=$(cat .github_api_token)
-read -p "Who owns the repo you want labels on?: " owner
-read -p "What repo do you want labels on?: " repo
-
-url="https://api.github.com/repos/${owner}/${repo}/labels"
-token_header="Authorization: token ${token}"
-accept_header="Accept: application/vnd.github.symmetra-preview+json"
-
-###
-# Create or Update labels
-###
-
-for i in ${!names[@]}; do
-    data="{\"name\":\"${names[$i]}\", \"color\":\"${colors[$i]}\", \"description\":\"${descriptions[$i]}\"}"
-
-    curl_output=$(curl -s -H "${token_header}" -H "${accept_header}" -X POST "${url}" -d "${data}")
-    curl_has_error=$(echo "${curl_output}" | jq -r '.errors')
-
-    if [ ! -z "${curl_has_error}" ] && [ "${curl_has_error}" != null ]; then
-        error=$(echo "${curl_output}" | jq -r '.errors[0].code')
-
-        if [ "${error}" == "already_exists" ]; then
-            echo "'${names[$i]}' already exists. Updating..."
-            curl_output=$(curl -s -H "${token_header}" -H "${accept_header}" -X PATCH "${url}/${names[$i]/ /%20}" -d "${data}")
-            echo "Updated '${names[$i]}'."
-        else
-            echo "Unknown error: ${error}"
-            echo -e "Output from curl: \n${curl_output}"
-            echo "Exiting..."
-            exit 1
-        fi
-    else
-        echo "Created '${names[$i]}'."
-    fi
-done
+create_or_update
 exit 0
